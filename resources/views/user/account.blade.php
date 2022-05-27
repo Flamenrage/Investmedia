214f267906c619e6da2b8932ac05a0b0a8d4055d
Flamenrage <55380311+Flamenrage@users.noreply.github.com>
2022-05-27 22:18:54 +0400

    yandex maps add

--- resources/views/user/account.blade.php
+++ resources/views/user/account.blade.php
@@ -3,6 +3,40 @@
 @section('title', 'Blog ' . $user->name)
 
 @section('page-title')
+    <script src="https://api-maps.yandex.ru/2.1?apikey=1389bb49-9aa2-41c1-aa93-27f43be7b6fa&lang=ru_RU" type="text/javascript"></script>
+    <script>
+        function init() {
+            let x = 54.19;
+            let y = 48.23;
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            }
+            function showPosition(position) {
+                x = position.coords.latitude.toFixed(2);
+                y = position.coords.longitude.toFixed(2);
+            }
+            var myMap = new ymaps.Map('map', {
+                center: [x, y],
+                zoom: 13,
+                controls: []
+            });
+
+            // Создадим экземпляр элемента управления «поиск по карте»
+            // с установленной опцией провайдера данных для поиска по организациям.
+            var searchControl = new ymaps.control.SearchControl({
+                options: {
+                    provider: 'yandex#search'
+                }
+            });
+
+            myMap.controls.add(searchControl);
+
+            // Программно выполним поиск определённых кафе в текущей
+            // прямоугольной области карты.
+            searchControl.search('Банк');
+        }
+        ymaps.ready(init);
+    </script>
     <div class="page-title db">
         <div class="container">
             <div class="row">
@@ -74,7 +108,10 @@
             @else
                 <h4> Комментарии пока отсутствуют. </h4>
             @endif
+            <h3 class="mt-3" style="text-align: center">Карта ближайших банков:</h3>
+            <div id="map" style="height:500px;width: 630px;"></div>
             <hr class="invis1">
+
         </div>
     </div>
 
